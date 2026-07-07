fa04a88e8f52a48d4f7b320b78e2fa1ba541f7eb
IceRamyeon <bladenova01@gmail.com>
2026-07-07 23:16:04 +0900

    changed time

--- Rendezvous_Simulation/main_MaxAcc.m
+++ Rendezvous_Simulation/main_MaxAcc.m
@@ -21,7 +21,7 @@ cfg.tf = 40;
 cfg.pause_t = 0.005;        
 cfg.skip_frame = 50;       
 cfg.stop_condition = 0;    
-cfg.auto_save = 1;         
+cfg.auto_save = 0;         
 
 % [제어 및 물리적 제한 가속도] 
 cfg.limit_acc = 1;         % 1G 제한 상태에서 클리핑 거동 확인!
